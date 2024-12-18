49c9b61988781a48dfafe1ff3883b5a4e7774419
Raouf-blip <abdoul-raouf.temirov2@etu.univ-lorraine.fr>
2024-12-18 11:25:48 +0100

    début flèche

--- umlgen/src/umlgen/fleches/FlecheAssociation.java
+++ umlgen/src/umlgen/fleches/FlecheAssociation.java
@@ -1,4 +1,14 @@
 package umlgen.fleches;
 
+import umlgen.ClasseEntiere;
+
 public class FlecheAssociation extends Fleche{
+
+    public FlecheAssociation(ClasseEntiere s, ClasseEntiere c){
+        super(s, c);
+    }
+
+    public String getTypeRelation(){
+        return "Association";
+    }
 }
--- umlgen/src/umlgen/fleches/FlecheImplementation.java
+++ umlgen/src/umlgen/fleches/FlecheImplementation.java
@@ -1,4 +1,14 @@
 package umlgen.fleches;
 
+import umlgen.ClasseEntiere;
+
 public class FlecheImplementation extends Fleche{
+
+    public FlecheImplementation(ClasseEntiere s, ClasseEntiere c){
+        super(s, c);
+    }
+
+    public String getTypeRelation(){
+        return "Implementation";
+    }
 }
